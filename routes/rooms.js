511db8347ae31f683967907228642551b9902e2c
alpha2207_dev <ayan787ali.mev@gmail.com>
2022-09-03 12:47:21 +0530

    added rom model,controller and routes

--- routes/rooms.js
+++ routes/rooms.js
@@ -1,25 +1,28 @@
-import mongoose from "mongoose";
-const RoomSchema = new mongoose.Schema(
-  {
-    title: {
-      type: String,
-      required: true,
-    },
-    price: {
-      type: Number,
-      required: true,
-    },
-    maxPeople: {
-      type: Number,
-      required: true,
-    },
-    desc: {
-      type: String,
-      required: true,
-    },
-    roomNumbers: [{ number: Number, unavailableDates: {type: [Date]}}],
-  },
-  { timestamps: true }
-);
+import express from "express";
+import {
+  createRoom,
+  deleteRoom,
+  getRoom,
+  getRooms,
+  updateRoom,
+  updateRoomAvailability,
+} from "../controllers/roomsControllers.js";
+import { verifyAdmin } from "../utils/verifyToken.js";
 
-export default mongoose.model("Room", RoomSchema);
+const router = express.Router();
+//CREATE
+router.post("/:hotelid", verifyAdmin, createRoom);
+
+//UPDATE
+router.put("/availability/:id", updateRoomAvailability);
+router.put("/:id", verifyAdmin, updateRoom);
+//DELETE
+router.delete("/:id/:hotelid", verifyAdmin, deleteRoom);
+//GET
+
+router.get("/:id", getRoom);
+//GET ALL
+
+router.get("/", getRooms);
+
+export default router;
